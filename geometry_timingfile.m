b3b5cfd36cdd217756352e8cea71fd8223018a15
Max Pensack <mpensack@gmail.com>
2021-02-23 15:50:58 -0500

    Added all task-related files (no data, analysis scripts, or figures).

---
+++ geometry_timingfile.m
@@ -0,0 +1,323 @@
+if ~exist('eye_','var'), error('This task requires eye signal input. Please set it up or try the simulation mode.'); end
+hotkey('x', 'escape_screen(); assignin(''caller'',''continue_'',false);');
+bhv_code(10,'FP1',11,'Fixation Acquired',20,'Stimulus',30,'FP2',40,'CC Trial',42,'Switch Trial',45, 'None Trial',46,'Break Fix',50,'Targets On',55,'Early Answer',60,'Go',64,'Double Saccade',65,'Break Choice',66,'Choice Made', 70,'Pre-Reward',99,'Reward'); % behavioral codes
+trialerror(0, 'Correct', 1, 'Alternative', 2, 'Across', 3, 'Random', 4, 'No Fixation', 5, 'Break Fix', 6, 'Early Answer', 7, 'No Choice/ Break Choice', 8, 'Incorrect, CL', 9, 'Correct, CL'); % error codes
+editable('weight','fix_radius','wait_for_fix','fix_time','stim_time','stim_trace_time','ctx_cue_time','ctx_cue_trace_time','max_decision_time','decision_fix_time','decision_trace_time','solenoid_time','big_drops','drop_gaps','training_rewards', 'time_out', 'performance_thresh', 'hint');
+persistent CC_trials
+persistent reward_count
+persistent directions
+
+if isempty(CC_trials)
+    CC_trials = [];
+    
+    directions = [];
+end
+
+if isempty(reward_count)
+    reward_count = 0;
+else
+    reward_count = [reward_count (reward_count(end) + sum(TrialRecord.LastTrialCodes.CodeNumbers==99))];
+end
+TrialRecord.User.reward_count = reward_count;
+dashboard(3, sprintf(['Total Reward: ' num2str(reward_count(end)) ' drops, %.2f drops/ trial'], (reward_count(end)./ TrialRecord.CurrentTrialNumber)), [0 255 255]);
+
+% PARAMETERS
+% Time intervals (in ms):
+wait_for_fix = 5000;
+fix_time = 500;
+stim_time = 300;
+stim_trace_time = 1000;
+ctx_cue_time = 300;
+ctx_cue_trace_time = [180 230]; % time maintain fixation after ctx_cue
+max_decision_time = 1500;
+double_thresh = 500; % threshold to prevent double saccades
+decision_fix_time = 100; % time to register choice
+decision_trace_time = 0; % period before reward delivery
+time_out = 2000; % increased ITI for wrong answers
+
+% Training variables:
+weight = NaN;
+TrialRecord.User.weight = weight;
+fix_radius = 2.5; % degrees
+performance_thresh = 0; % running HR threshold to turn off ctx_cue
+performance_window = 10; % compute running HR based on n trials back
+hint = 10; % # trials to show context cue independent of performance
+
+% Reward variables:
+training_rewards = [0 0 0 0 0]; % change to 1 to turn on training rewards for given scene (1-5)
+solenoid_time = 200; %ms
+drop_gaps = 0; %ms
+little_drops = 1; %number of pulses
+big_drops = 2; %number of pulses
+
+% TaskObjects defined in the conditions file:
+fixation_point = 1;
+FP_background = 2;
+stimulus = 3;
+ctx_cue = 4;
+target = 5;
+off_target1 = 6;
+off_target2 = 7;
+off_target3 = 8;
+
+% SCENES:
+% scene 1: fixation
+fix1 = SingleTarget(eye_);
+fix1.Target = fixation_point;
+fix1.Threshold = fix_radius;
+wth1 = WaitThenHold(fix1);       
+wth1.WaitTime = wait_for_fix;
+wth1.HoldTime = fix_time;
+scene1 = create_scene(wth1,fixation_point); 
+
+% scene 2: stimulus
+fix2 = SingleTarget(eye_);
+fix2.Target = fixation_point;
+fix2.Threshold = fix_radius;
+wth2 = WaitThenHold(fix2);
+wth2.WaitTime = 0; % Fixation is already acquired, so don't wait.
+wth2.HoldTime = stim_time;
+scene2 = create_scene(wth2, [fixation_point FP_background stimulus]);
+
+% scene 3: stimulus trace
+fix3 = SingleTarget(eye_);
+fix3.Target = fixation_point;
+fix3.Threshold = fix_radius;
+wth3 = WaitThenHold(fix3);
+wth3.WaitTime = 0;
+wth3.HoldTime = stim_trace_time;
+scene3 = create_scene(wth3,fixation_point);
+
+% scene 4: context/ switch/ none cue
+fix4 = SingleTarget(eye_);
+fix4.Target = fixation_point;
+fix4.Threshold = fix_radius;
+wth4 = WaitThenHold(fix4);
+wth4.WaitTime = 0;
+wth4.HoldTime = ctx_cue_time;
+try
+    if hit_rate(TrialRecord.TrialErrors(end-performance_window:end))>=performance_thresh && TrialRecord.CurrentTrialWithinBlock>hint
+        CC_trials = [CC_trials 0];
+        dashboard(2, 'None Trial', [255 255 255])
+        scene4 = create_scene(wth4,[fixation_point FP_background]);
+    else
+        CC_trials = [CC_trials 1];
+        dashboard(2, 'CC Trial',[255 0 255])
+        scene4 = create_scene(wth4,[fixation_point FP_background ctx_cue]);
+    end
+catch
+    CC_trials = [CC_trials 1];
+    dashboard(2, 'CC Trial',[255 0 255])
+    scene4 = create_scene(wth4,[fixation_point FP_background ctx_cue]);
+end
+
+TrialRecord.User.CC_trials = CC_trials;
+
+% scene 5: context trace, targets on
+fix5 = SingleTarget(eye_);
+fix5.Target = fixation_point;
+fix5.Threshold = fix_radius;
+wth5 = WaitThenHold(fix5);
+wth5.WaitTime = 0;
+wth5.HoldTime = exprnd(12)+ ctx_cue_trace_time(1); % add noise from exponential distribution (i.e. flat hazard rate)
+if wth5.HoldTime > ctx_cue_trace_time(2)
+    wth5.HoldTime = ctx_cue_trace_time(2); % don't let the interval get too long
+end
+disp(wth5.HoldTime)
+scene5 = create_scene(wth5,[fixation_point target off_target1 off_target2 off_target3]);
+
+% scene 6: choice
+mul6 = MultiTarget(eye_);
+mul6.Target = [target off_target1 off_target2 off_target3];
+mul6.Threshold = fix_radius;
+mul6.WaitTime = max_decision_time;
+mul6.HoldTime = decision_fix_time;
+mul6.TurnOffUnchosen = false;
+scene6 = create_scene(mul6,[target off_target1 off_target2 off_target3]);
+
+
+% TASK:
+%Dashboard settings
+try
+    running_HR = hit_rate(TrialRecord.TrialErrors(end-performance_window:end));
+catch
+    running_HR = hit_rate(TrialRecord.TrialErrors);
+end
+
+dashboard(1, sprintf([num2str(performance_window) '-Trial HR: %.2f, Overall HR: %.2f'], running_HR*100, hit_rate(TrialRecord.TrialErrors)*100));
+if TrialRecord.User.CL
+    dashboard(4, 'Correction Loop!', [255 0 0])
+else
+    dashboard(4, 'Good Monkey', [0 255 0])
+end
+
+direction_count = [sum(directions==1) sum(directions==2) sum(directions==3) sum(directions==4)];
+dashboard(5, ['Choices [Up Right Down Left]: ' num2str(direction_count)], [255 255 0]); 
+
+directions = [directions 0];
+
+% scene 1: fixation
+run_scene(scene1,10);
+if ~wth1.Success
+    idle(0);
+    if wth1.Waiting
+        trialerror(4); % No Fixation
+    else
+        trialerror(5); % Break Fixation
+        eventmarker(46);
+    end
+    return
+else
+    eventmarker(11) % Fixation acquired
+    if training_rewards(1), goodmonkey(solenoid_time, 'numreward', little_drops); end
+end
+
+% scene 2: stimulus
+run_scene(scene2,20);
+if ~wth2.Success
+    idle(0);
+    trialerror(5); % Break Fixation
+    eventmarker(46);
+    return
+else
+    if training_rewards(2), goodmonkey(solenoid_time, 'numreward', little_drops); end
+end
+
+% scene 3: stimulus trace
+run_scene(scene3,30);
+if ~wth3.Success
+    idle(0);
+    trialerror(5); % Break Fixation
+    eventmarker(46);
+    return
+else
+    if training_rewards(3), goodmonkey(solenoid_time, 'numreward', little_drops); end
+end
+
+% scene 4: context cue
+if TrialRecord.User.CC_trials(end)
+    run_scene(scene4,40);
+else
+    run_scene(scene4,45);
+end
+if ~wth4.Success
+    idle(0);
+    trialerror(5); % Break Fixation
+    eventmarker(46);
+    return
+else
+    if training_rewards(4), goodmonkey(solenoid_time, 'numreward', little_drops); end
+end
+
+% scene 5: context trace, targets on
+run_scene(scene5, 50);
+if ~wth5.Success
+    idle(0);
+    trialerror(6); % Early Answer
+    eventmarker(55) % Early Answer
+    return
+else
+    if training_rewards(5), goodmonkey(solenoid_time, 'numreward', little_drops); end
+end
+
+% scene 6: choice
+run_scene(scene6, 60);
+rt = mul6.RT;
+
+if ~mul6.Waiting
+    eventmarker(66) % Choice Made 
+end
+
+if ~mul6.Success
+    idle(0);
+    if mul6.Waiting
+        trialerror(7); % No Choice
+    else
+        trialerror(7); % Break Choice
+        eventmarker(65)
+    end
+    return
+end
+
+% Rewards
+if target==mul6.ChosenTarget
+    if TrialRecord.User.CL
+            trialerror(9); % Correct CL trial
+            idle(decision_trace_time,[], 70)
+            goodmonkey(solenoid_time, 'numreward', big_drops, 'pausetime', drop_gaps, 'eventmarker',99);
+    else
+        trialerror(0); % Correct
+        idle(decision_trace_time,[], 70)
+        goodmonkey(solenoid_time, 'numreward', big_drops, 'pausetime', drop_gaps, 'eventmarker',99);
+    end
+    
+    %Code Direction - Correct
+    if TrialRecord.CurrentCondition == 1 || TrialRecord.CurrentCondition == 8
+        directions(end) = 1;
+    elseif TrialRecord.CurrentCondition == 2 || TrialRecord.CurrentCondition == 7
+        directions(end) = 3;
+    elseif TrialRecord.CurrentCondition == 3 || TrialRecord.CurrentCondition == 5
+        directions(end) = 2;
+    elseif TrialRecord.CurrentCondition == 4 || TrialRecord.CurrentCondition == 6
+        directions(end) = 4;
+    end
+elseif off_target1==mul6.ChosenTarget
+    if TrialRecord.User.CL
+        trialerror(8); % Incorrect CL trial
+        idle(time_out)
+    else
+        idle(time_out)
+        trialerror(1); % Incorrect, correct in other context
+    end
+    
+    %Code Direction - Other Context
+    if TrialRecord.CurrentCondition == 1 || TrialRecord.CurrentCondition == 7
+        directions(end) = 2;
+    elseif TrialRecord.CurrentCondition == 2 || TrialRecord.CurrentCondition == 8
+        directions(end) = 4;
+    elseif TrialRecord.CurrentCondition == 3 || TrialRecord.CurrentCondition == 6
+        directions(end) = 3;
+    elseif TrialRecord.CurrentCondition == 4 || TrialRecord.CurrentCondition == 5
+        directions(end) = 1;
+    end
+elseif off_target2==mul6.ChosenTarget
+    if TrialRecord.User.CL
+        trialerror(8); % Incorrect CL trial
+        idle(time_out)
+    else
+        idle(time_out)
+        trialerror(2) % Incorrect, across FP from target
+    end
+    
+    %Code Direction - Across
+    if TrialRecord.CurrentCondition == 1 || TrialRecord.CurrentCondition == 8
+        directions(end) = 3;
+    elseif TrialRecord.CurrentCondition == 2 || TrialRecord.CurrentCondition == 7
+        directions(end) = 1;
+    elseif TrialRecord.CurrentCondition == 3 || TrialRecord.CurrentCondition == 5
+        directions(end) = 4;
+    elseif TrialRecord.CurrentCondition == 4 || TrialRecord.CurrentCondition == 6
+        directions(end) = 2;
+    end
+elseif off_target3==mul6.ChosenTarget
+    if TrialRecord.User.CL
+        trialerror(8); % Incorrect CL trial
+        idle(time_out)
+    else
+        idle(time_out)
+        trialerror(3) % Incorrect, random guess
+    end
+    
+    %Code Direction - Random
+    if TrialRecord.CurrentCondition == 1 || TrialRecord.CurrentCondition == 7
+        directions(end) = 4;
+    elseif TrialRecord.CurrentCondition == 2 || TrialRecord.CurrentCondition == 8
+        directions(end) = 2;
+    elseif TrialRecord.CurrentCondition == 3 || TrialRecord.CurrentCondition == 6
+        directions(end) = 1;
+    elseif TrialRecord.CurrentCondition == 4 || TrialRecord.CurrentCondition == 5
+        directions(end) = 3;
+    end
+end
+
+TrialRecord.User.directions = directions;
---
+++ geometry_userloop.m
@@ -0,0 +1,240 @@
+function [C,timingfile,userdefined_trialholder] = geometry_userloop(MLConfig,TrialRecord)
+%MJP 11/11/2020
+
+% Training variables
+z = 70; % Number of correct trials before block (context) switch
+sequence_depth = 2; % Number of times each condition should be shown in a given trial sequence
+CL_threshold = -3; % Number of errors for a given condition before starting correction loop
+CL_depth = 3; % Number of times condition will be repeated in CL
+n_fractals = 4; % 1-4, set to 4 for full set of fractals
+%training = 5; % 1-8, SINGLE-CONDITION TRAINING, SEE CAPS COMMENT BELOW TOO!!
+contrast = 1; % 0-1 for off-target FPs
+instructed_threshold = 10; % Adjust contrast after threshold errors in a row, only in CL
+
+% Initialize ML variables
+C = [];
+timingfile = 'geometry_timingfile.m';
+userdefined_trialholder = '';
+
+persistent condition_correct % number of time each fractal has been completed successfully
+persistent condition_incorrect % number of errors for condition
+persistent correct_counts % running tally correct of answers
+persistent incorrect_counts % running tally of incorrect answers
+persistent CL_counter % [number of repeats left in CL, condition number]
+persistent CL_errors % counts mistakes within correction loop
+persistent CL_trials
+persistent first_block
+persistent timing_filename_returned
+if isempty(condition_correct) || TrialRecord.BlockChange %Reset counters if start of session or block change happened
+    condition_correct = zeros(1,2*n_fractals);
+    condition_incorrect = zeros(1,2*n_fractals);
+    correct_counts = zeros(1,2*n_fractals);
+    incorrect_counts = zeros(1,2*n_fractals);
+    CL_counter = [0 0]; 
+    CL_errors = 0;
+    CL_trials = [];
+end
+if isempty(timing_filename_returned)
+    timing_filename_returned = true;
+    return
+end
+
+TrialRecord.User.correct_counts = correct_counts;
+TrialRecord.User.incorrect_counts = incorrect_counts;
+
+% Trial Conditions
+target_distance = 7; %degrees from center, for all target locations (up, right, down, left)
+up = [0 target_distance];
+right = [target_distance 0];
+down = [0 -target_distance];
+left = [-target_distance 0];
+
+image_list = {'stim_0.png','stim_1.png','stim_2.png', 'stim_3.png', 'stim_4.png', 'stim_5.png'};
+conditions =... %Context 1: rows 1-4, Context 2: rows 5-8
+   [1 5 up right down left;  % [stimulus ctx_cue target off_target1 off_target2 off_target3]
+    3 5 down left up right;  % off_target1 = correct in other context
+    2 5 right down left up;  % off_target2 = across FP from target
+    4 5 left up right down;  % off_target3 = random guess
+    1 6 right up left down;
+    3 6 left down right up;
+    2 6 down right up left;
+    4 6 up left down right];
+
+%Randomly select block if first trial
+if isempty(TrialRecord.TrialErrors)
+    first_block = randi([0,1]);
+end
+
+%Increase the block number after z correct trials
+% correct_trial_count = sum(0==TrialRecord.TrialErrors);
+% block = mod(floor((correct_trial_count+(z^first_block))/z),2) + 1;
+block = 2; %for training
+
+%Increase sequence counter based on last trial
+try
+    if TrialRecord.TrialErrors(end)==0 && CL_counter(1)==0 %don't count CL trials as correct
+        condition_correct(TrialRecord.CurrentCondition) = condition_correct(TrialRecord.CurrentCondition)+1;
+        correct_counts(TrialRecord.CurrentCondition) = correct_counts(TrialRecord.CurrentCondition)+1;
+        condition_incorrect(TrialRecord.CurrentCondition) = 0; % Reset incorrect count if correct trial is performed, remove this line to have cumulative CL criteria
+        disp(['condition correct: ' num2str(condition_correct)])
+    elseif ismember(TrialRecord.TrialErrors(end), [1 2 3]) && CL_counter(1)==0
+        condition_incorrect(TrialRecord.CurrentCondition) = condition_incorrect(TrialRecord.CurrentCondition)-1;
+        incorrect_counts(TrialRecord.CurrentCondition) = incorrect_counts(TrialRecord.CurrentCondition)-1;
+        disp(['condition incorrect: ' num2str(condition_incorrect)])
+        disp(['incorrect counts: ' num2str(incorrect_counts)])
+    end
+    
+    %Initiate CL, if needed
+    if condition_incorrect(TrialRecord.CurrentCondition) == CL_threshold
+        condition_incorrect(TrialRecord.CurrentCondition) = 0; % Reset error count
+        CL_counter = [CL_depth TrialRecord.CurrentCondition];
+        CL_errors = 0;
+    end
+catch    
+end
+
+%Reset sequence count if each fractal has been encountered enough times
+if sum(condition_correct(1,(1:4)+((block-1)*4))) == sequence_depth*n_fractals
+    condition_correct = zeros(1,2*n_fractals);
+end
+
+%Update CL_counter
+try
+    if TrialRecord.TrialErrors(end)==9
+        CL_counter(1) = CL_counter(1)-1;
+        if CL_counter(1) == 0
+            CL_errors = 0;
+            disp('CL Over')
+        else
+            disp(['CL trials left: ' num2str(CL_counter(1))])
+        end
+    elseif TrialRecord.TrialErrors(end)==8
+        CL_counter(1) = CL_depth; % Require n correct in a row to exit CL
+        CL_errors = CL_errors + 1;
+        disp(['CL errors: ' num2str(CL_errors)])
+    end
+catch
+end
+
+%Enter CL if criteria are met
+if CL_counter(1) ~= 0
+    condition = CL_counter(2);
+    chosen_condition = conditions(condition,:);
+    TrialRecord.User.CL = 1;
+    CL_trials = [CL_trials 1];
+    pick_condition = 0;
+else
+    pick_condition = 1;
+end
+
+%Pick fractal
+while pick_condition
+    condition = randi(n_fractals)+((block-1)*4);
+    if condition_correct(condition) < sequence_depth
+        chosen_condition = conditions(condition,:);
+        pick_condition = 0;
+        TrialRecord.User.CL = 0;
+        CL_trials = [CL_trials 0];
+    end
+end
+
+TrialRecord.User.CL_trials = CL_trials;
+
+%Adjust contrast on instructed trials within CL
+if CL_errors >= instructed_threshold
+    contrast = 0.2;
+%       %To reset counter after correct CL trials
+%     if TrialRecord.TrialErrors(end) == 9
+%         CL_errors = 0;
+%     end
+end
+
+%chosen_condition = conditions(training,:); % SINGLE-CONDITION TRAINING, IF USING, COMMENT OUT NEXT LINE
+
+
+% Set the stimuli
+target_color = [255 255 255];
+off_target_color = [contrast*255 contrast*255 contrast*255];
+
+stimulus = image_list{chosen_condition(1)};
+ctx_cue = image_list{chosen_condition(2)};
+target_x = chosen_condition(3);
+target_y = chosen_condition(4);
+off1_x = chosen_condition(5);
+off1_y = chosen_condition(6);
+off2_x = chosen_condition(7);
+off2_y = chosen_condition(8);
+off3_x = chosen_condition(9);
+off3_y = chosen_condition(10);
+
+% TaskObjects:
+%1: fixation_point
+C(1).Type = 'crc';
+C(1).Radius = 0.1;     % visual angle
+C(1).Color = [255 255 255];  % [R G B]
+C(1).FillFlag = 1;
+C(1).Xpos = 0;
+C(1).Ypos = 0;
+
+%2: FP_background
+C(2).Type = 'crc';
+C(2).Radius = 0.5;     % visual angle
+C(2).Color = [0 0 0];  % [R G B]
+C(2).FillFlag = 1;
+C(2).Xpos = 0;
+C(2).Ypos = 0;
+
+%3: stimulus
+C(3).Type = 'pic';
+C(3).Name = stimulus;
+C(3).Xpos = 0;
+C(3).Ypos = 0;
+C(3).Colorkey = [0 0 0]; %sets black as transparent
+
+%4: ctx_cue
+C(4).Type = 'pic';
+C(4).Name = ctx_cue;
+C(4).Xpos = 0;
+C(4).Ypos = 0;
+C(4).Colorkey = [0 0 0]; %sets black as transparent
+
+%5: target
+C(5).Type = 'crc';
+C(5).Radius = 0.1;     % visual angle
+C(5).Color = target_color;  % [R G B]
+C(5).FillFlag = 1;
+C(5).Xpos = target_x;
+C(5).Ypos = target_y;
+
+%6: off_target1
+C(6).Type = 'crc';
+C(6).Radius = 0.1;     % visual angle
+C(6).Color = off_target_color;  % [R G B]
+C(6).FillFlag = 1;
+C(6).Xpos = off1_x;
+C(6).Ypos = off1_y;
+
+%7: off_target2
+C(7).Type = 'crc';
+C(7).Radius = 0.1;     % visual angle
+C(7).Color = off_target_color;  % [R G B]
+C(7).FillFlag = 1;
+C(7).Xpos = off2_x;
+C(7).Ypos = off2_y;
+
+%8: off_target3
+C(8).Type = 'crc';
+C(8).Radius = 0.1;     % visual angle
+C(8).Color = off_target_color;  % [R G B]
+C(8).FillFlag = 1;
+C(8).Xpos = off3_x;
+C(8).Ypos = off3_y;
+
+% Set the block number and the condition number of the next trial. Since
+% this userloop function provides the list of TaskObjects and timingfile
+% names, ML does not need the block/condition number. They are just for
+% your reference.
+% However, if TrialRecord.NextBlock is -1, the task ends immediately
+% without running the next trial.
+TrialRecord.NextBlock = block;
+TrialRecord.NextCondition = condition;
