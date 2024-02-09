7b5629b7f67470ea3b3b5581f3607947f9226a4f
Max Pensack <ghd2107@columbia.edu>
2024-02-09 14:30:25 -0500

    decreased block_length for geo_train_task

--- geometry_train/geometry_train_userloop.m
+++ geometry_train/geometry_train_userloop.m
@@ -2,7 +2,7 @@ function [C,timingfile,userdefined_trialholder] = geometry_train_userloop(MLConf
 %MJP 11/11/2020
 
 % Training variables
-block_length = 250; % Number of trials before context switch
+block_length = 200; % Number of trials before context switch
 sequence_depth = 2; % Number of times each condition should be shown in a given trial sequence
 CL_threshold = -1; % Number of errors for a given condition before starting correction loop
 CL_depth = 1; % Number of times condition will be repeated in CL
